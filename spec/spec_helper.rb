d1395333651df49166231e2dc3d6ea65dff3554b
onigra <3280467rec@gmail.com>
2016-05-23 18:33:10 +0900

    remove rspec/autorun

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require "rspec"
 require "logger/ltsv"
-require "rspec/autorun"
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
